7f1be326da9fdb695876cc392ea21da404bb5bd9
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-12-22 14:47:37 +0530

    Updated Valid Task

--- src/reducer.js
+++ src/reducer.js
@@ -16,31 +16,32 @@ export default function taskReducer(state,action){
             ? { ...task, task: action.input } 
             : task  
         );
-        return state
       }
+      return state
     }
     
     case "DELETE" : {
       if(isValidId(state, action.id)){
         state = state.filter((data) => data.id !== action.id);
-          return state  
       }
+      return state  
     }
     case "STATUS_CHANGE" :  { 
-      if(isValidId(state, action.id)){
-      state = state.map((task) => {
-      if (task.id === action.id) {
-        return {
-          ...task,
-          status: task.status == "active" ? "completed" : "active",
-        };
+        if(isValidId(state, action.id)){
+        state = state.map((task) => {
+        if (task.id === action.id) {
+          return {
+            ...task,
+            status: task.status == "active" ? "completed" : "active",
+          };
+          }
+         return task
+        })
       }
-      return task
-    })
-    return state
-  }
+  return state
   }
     case "ADD_TASK" : {
+      if(!isValidTask(action.task)){return state}
       state = [...state, {...action.task, id:genId(state)}]
       return state
     }
@@ -53,6 +54,9 @@ function genId(tasks){
       max = Math.max(i.id , max)
   }
   return max == -Infinity ? 0 : max+1
+}
+function isValidTask(task){
+  return task.task.length>0 && (task.status =="completed" || task.status == "active") && task.color.length>0
 }
 function isValidId(tasks,id){
   for(let i of tasks){
